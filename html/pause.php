f23d875f7fa2bd09521e612c032d64e0cc9a0f6c
Geoffroey-Allen Franklin <gfranklin@atsu.edu>
2018-12-19 12:35:07 -0500

    Adds manual link to pause.php for users who get stuck. See issue #52

--- html/pause.php
+++ html/pause.php
@@ -8,16 +8,18 @@ include("header.php");
 $redirect = isset($_SESSION['redirect']) ? $_SESSION['redirect'] : 'index.php';
 	echo "<meta http-equiv=\"Refresh\" content=\"5; url=$redirect\">";
 	unset($_SESSION['redirect']);
-	
+
 echo "
 <div class=\"main\">
 	<span class=\"left-box\">
 		<center>
-		<img src=\"images/updating.gif\"> 
+		<img src=\"images/updating.gif\">
 		</center>
 	</span>
 	<span class=\"left-box\">
 		<font size=\"+2\">Processing Request, please wait.</font>
+		<br /> <br /> <br />
+		<a href=\"$redirect\">Please, click here if you are not redirected in a few moments.</a>
 	</span>
 ";
 
